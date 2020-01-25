c9970e05bd7c76784de61135a4ccea05e2fc6c29
mohitgarg <mohitgarg083@gmail.com>
2020-01-25 18:30:41 +0530

    155

--- src/index.js
+++ src/index.js
@@ -15,8 +15,9 @@ const publicDirectoryPath=path.join(__dirname,"../public")
 
 app.use(express.static(publicDirectoryPath))
 
-io.on("connection",()=>{
+io.on("connection",(socket)=>{
     console.log("New web socket connection");
+    socket.emit("countUpdated")
     
 })
 
